b2768c50302833e9dcaf69680d66d4e3a9a2153f
aludi <ludivanleeuwen@gmail.com>
2021-06-12 17:30:33 +0200

    parse tree metrics start

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,9 @@
 import benepar
 from numpy import average
 import spacy
+from nltk import Tree, acyclic_breadth_first
 
-# benepar.download('benepar_en3')
+#benepar.download('benepar_en3')
 
 def compute_density_metrics(sent):
     densities = {'DET':0, 'AUX':0,'CONJ':0, 'PRON':0, 'ADP':0, 'PUNCT':0, 'FUNC':0}
@@ -40,6 +41,23 @@ def get_const_size(sent, label):
     if const_num > 0 : average /= const_num
     return max_s, average
 
+
+def tok_format(tok):
+    return "_".join([tok.orth_, tok.tag_])
+
+def to_nltk_tree(node, left, right):
+    left = left + node.n_lefts
+    right = right + node.n_rights
+    print(node.text)
+    print(left, right)
+    if node.n_lefts + node.n_rights > 0:
+        return Tree(tok_format(node), [to_nltk_tree(child, left, right) for child in node.children])
+    else:
+        return tok_format(node)
+
+
+
+
 def compute_constituents_metrics(sent):
     constituents = {'S_len': len(sent), 'Depth': walk_tree(sent.root,1)}
     constituents['NP_max'], constituents['NP_avg'] = get_const_size(sent, "NP")
@@ -48,6 +66,26 @@ def compute_constituents_metrics(sent):
     constituents['ADJP_max'], constituents['ADJP_avg'] = get_const_size(sent, "ADJP")
     return constituents
 
+def compute_parse_tree_metrics(sent):
+    constituent_iter = iter(sent._.constituents)
+    count_left = 0
+    count_right = 0
+    for x in constituent_iter:
+        if x._.parent != None:
+            if list(x)[len(list(x)) - 1] == list(x._.parent)[len(list(x._.parent)) - 1]:
+                count_right = count_right + 1
+            if list(x)[0] == list(x._.parent)[0]:
+                count_left = count_left + 1
+
+    if count_right != 0:
+        ratio = count_left/count_right
+    else:
+        ratio = 0
+
+    parse_tree = {'right_branches': count_right, 'left_branches': count_left, 'ratio': ratio}
+    return parse_tree
+
+
 nlp = spacy.load('en_core_web_md')
 if spacy.__version__.startswith('2'):
     nlp.add_pipe(benepar.BeneparComponent("benepar_en3"))
@@ -55,11 +93,45 @@ else:
     nlp.add_pipe("benepar", config={"model": "benepar_en3"})
 
 doc = nlp("The time for action is now.")
+doc = nlp("Every man asked some actress that he met about some play that she appeared in")
+#doc = nlp("I throw the ball to the dog")
+
+
 sent = list(doc.sents)[0]
+
+
+for sent in doc.sents:
+    for x in sent:
+        print(type(x))
+
+    print(sent._.parse_string)
+    print(type(sent))
+    iterat = iter(sent._.constituents)
+    count_left = 0
+    count_right = 0
+    for x in iterat:
+        print(x, "\t ", x._.parent)
+        if x._.parent != None:
+            print(list(x._.parent))
+            parent_list = list(x._.parent)
+            if list(x)[len(list(x))-1] == list(x._.parent)[len(list(x._.parent))-1]:
+                print("\t +1, right")
+                count_right = count_right + 1
+            if list(x)[0] == list(x._.parent)[0]:
+                print("\t +1, left")
+                count_left = count_left + 1
+  
+
+
+
 # print(sent._.parse_string)
 
+
+
 # print(compute_density_metrics(sent))
-print(compute_constituents_metrics(sent))
+#print(compute_constituents_metrics(sent))
+# print(compute_parse_tree_metrics(sent))
+
 
 
 
