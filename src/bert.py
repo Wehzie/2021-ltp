10ea537a7a04a743be63a9ecac0553c392dd1424
wehzie <39304339+Wehzie@users.noreply.github.com>
2021-06-15 23:03:29 +0200

    model saving, loading, prediction and analysis

--- src/bert.py
+++ src/bert.py
@@ -6,6 +6,7 @@ from pathlib import Path
 
 import pandas as pd
 import torch
+from sklearn.metrics import classification_report
 from simpletransformers.classification import ClassificationModel, ClassificationArgs
 
 sys.path.append(os.getcwd())
@@ -16,9 +17,18 @@ parser.add_argument('--nrows', default=None, type=int, nargs='?',
                     help='number of data rows')
 parser.add_argument('--epochs', default=1, type=int, nargs='?',
                     help='number of data rows')
+parser.add_argument('--override', default=False, type=bool, nargs='?',
+                    help='override model output directory')
 
 args = parser.parse_args()
 print(args)
+
+def load_model_pth(model_path: Path):
+    """
+    Load a pytorch model from a model.pth; including weights and model structure.
+    """
+    model = torch.load(model_path)
+    return model
 
 if __name__ == "__main__":
 
@@ -39,7 +49,10 @@ if __name__ == "__main__":
     test = data[int(size*0.85):]
 
     # Optional model configuration
-    model_args = ClassificationArgs(num_train_epochs=args.epochs)
+    model_args = ClassificationArgs(
+        num_train_epochs=args.epochs,
+        overwrite_output_dir=args.override
+    )
 
     # Create a ClassificationModel
     model = ClassificationModel(
@@ -54,5 +67,18 @@ if __name__ == "__main__":
 
     print(result)
 
-    # Make predictions with the model
-    # predictions, raw_outputs = model.predict(test['text'])
+    # save model weights and model structure
+    torch.save(model, 'data/model.pth')
+
+    # predict on test set
+    X_test = test["text"].values.tolist()
+    predictions, raw_outputs = model.predict(X_test)
+    test.insert(1, "pred", predictions)    
+
+    print(test.head())
+    
+    # save
+    test.to_csv(Path("data/test_pred.csv"))
+
+    # analyse
+    print(classification_report(test["labels"], test["pred"]))
