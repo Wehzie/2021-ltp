7c2869c8a42af3d3ca236e8b537ba4ecbacc1fe2
wehzie <39304339+Wehzie@users.noreply.github.com>
2021-06-15 21:00:07 +0200

    updated colab notebook and added commandline args to bert

--- src/bert.py
+++ src/bert.py
@@ -1,46 +1,58 @@
-from simpletransformers.classification import ClassificationModel, ClassificationArgs
-import pandas as pd
-import logging
-import os
+import argparse
 import sys
+import os
+import logging
 from pathlib import Path
+
+import pandas as pd
 import torch
+from simpletransformers.classification import ClassificationModel, ClassificationArgs
 
 sys.path.append(os.getcwd())
-
 from src.utils import make_bert_datasets
 
-logging.basicConfig(level=logging.INFO)
-transformers_logger = logging.getLogger("transformers")
-transformers_logger.setLevel(logging.WARNING)
+parser = argparse.ArgumentParser(description='Train and use Bert model.')
+parser.add_argument('--nrows', default=None, type=int, nargs='?',
+                    help='number of data rows')
+parser.add_argument('--epochs', default=1, type=int, nargs='?',
+                    help='number of data rows')
 
-dev_path = Path("data/dev/europarl_dev.csv")
+args = parser.parse_args()
+print(args)
 
-# read data
-data = pd.read_csv(dev_path, index_col=0, header = 0, nrows=20) 
+if __name__ == "__main__":
 
-# format and split data
-data = make_bert_datasets(data)
-size = len(data)
-train = data[:int(size*0.7)]
-dev = data[int(size*0.7):int(size*0.85)]
-test = data[int(size*0.85):]
+    logging.basicConfig(level=logging.INFO)
+    transformers_logger = logging.getLogger("transformers")
+    transformers_logger.setLevel(logging.WARNING)
 
-# Optional model configuration
-model_args = ClassificationArgs(num_train_epochs=1)
+    dev_path = Path("data/dev/europarl_dev.csv")
 
-# Create a ClassificationModel
-model = ClassificationModel(
-    "bert", "bert-base-cased", args=model_args, use_cuda=torch.cuda.is_available()
-)
+    # read data
+    data = pd.read_csv(dev_path, index_col=0, header = 0, nrows=args.nrows) 
 
-# Train the model
-model.train_model(train)
+    # format and split data
+    data = make_bert_datasets(data)
+    size = len(data)
+    train = data[:int(size*0.7)]
+    dev = data[int(size*0.7):int(size*0.85)]
+    test = data[int(size*0.85):]
 
-# Evaluate the model
-result, model_outputs, wrong_predictions = model.eval_model(dev)
+    # Optional model configuration
+    model_args = ClassificationArgs(num_train_epochs=1)
 
-print(result)
+    # Create a ClassificationModel
+    model = ClassificationModel(
+        "bert", "bert-base-cased", args=model_args, use_cuda=torch.cuda.is_available()
+    )
 
-# Make predictions with the model
-# predictions, raw_outputs = model.predict(test['text'])
+    # Train the model
+    model.train_model(train)
+
+    # Evaluate the model
+    result, model_outputs, wrong_predictions = model.eval_model(dev)
+
+    print(result)
+
+    # Make predictions with the model
+    # predictions, raw_outputs = model.predict(test['text'])
